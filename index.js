0e8023743f8733dd963ceb97fee63d23551dc5ca
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-11-10 13:15:07 +0200

    adding

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+require('dotenv').config();
+// Start up DB Server
+const { db } = require('./src/models/index.js');
+const  PORT  = process.env.PORT || 3000
+db.sync()
+  .then(() => {
+
+    // Start the web server
+    require('./src/server.js').startup(process.env.PORT);
+  }).catch((err) => {
+    console.log(err.message);
+  });
+ 
